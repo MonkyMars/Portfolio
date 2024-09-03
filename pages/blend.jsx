12b3a8f17800ec1081310fdf65c0042b04b40210
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-09-03 20:54:50 +0200

    improved about me text

--- pages/blend.jsx
+++ pages/blend.jsx
@@ -52,25 +52,25 @@ export default function Blend() {
         </nav>
 
         <header className={styles.header}>
-          <h1 ref={leviRef}>Levi Noppers</h1>
-          <code>Front end software developer</code>
+          <h1 ref={leviRef}>{'Levi Noppers'}</h1>
+          <code>{'Front end software developer'}</code>
           <div ref={headerRef}>
                 <Link href='https://github.com/MonkyMars' target="_blank">
                 <button>
                     <Image src='/github.png' alt='github' width={20} height={20} />
-                    Github
+                    {'Github'}
                 </button>
             </Link>
             <Link href={'/projects'}>
                 <button>
                     <Image src='/page.png' alt='home' width={20} height={20}/>
-                    Projects
+                    {'Projects'}
                 </button>
             </Link>
                 <Link href='/contact'>
                 <button className={styles.Contact}>
                     <Image src='/user-portfolio.png' alt='contact' width={20} height={20} />
-                    Contact
+                    {'Contact'}
                 </button>
             </Link>
                 </div>
@@ -79,20 +79,20 @@ export default function Blend() {
         <main className={styles.mainContent}>
           <div>
             <h2>About me</h2>
-            <p>{"Hello, World! I'm Levi Noppers, a 15-year-old software engineer with a passion for developing innovative web applications."}</p>
+            <p>{"Hello World! I'm Levi Noppers, a 15-year-old software engineer from The Hague, the Netherlands, with a passion for developing web applications."}</p>
 
-            <p>{"Over the past two years, I’ve dedicated myself to learning and mastering the art of coding, with a strong focus on building full-stack applications. My journey in the tech world began with curiosity, and it quickly evolved into a deep-seated passion for creating digital solutions."}</p>
+            <p>{"Over the past two years, I’ve dedicated myself to learn coding, I mainly focus on the front-end. My journey as a software developer began when I was 12 and wanted to create my first Discord Bot, since then I've improved a lot at coding, day in and day out spent on coding new web-apps"}</p>
 
-            <p>{"I specialize in crafting both the front-end and back-end of web applications, ensuring that every project I work on is fully functional and aesthetically pleasing. My experience includes developing robust APIs, designing efficient databases, and, most importantly, creating intuitive and engaging user interfaces. The front-end is where my creativity truly shines—I love experimenting with new ideas, pushing the boundaries of design, and bringing concepts to life through code."}</p>
+            <p>{"I specialize in the front-end and a little back-end, making sure that every project I work on looks good and works as intended. My experience includes developing multiple fully-functional websites, creating a database-API."}</p>
 
-            <p>{"When I'm not working on a project, you'll likely find me brainstorming new ideas or diving into the latest web development trends. I’m constantly seeking to improve my skills and expand my knowledge, whether it’s by learning new programming languages, exploring different frameworks, or participating in coding communities."}</p>
+            <p>{"I’m open to learning and improving my skills, whether it’s by exploring a new framework, or finding better solutions to my problems"}</p>
 
-            <p>{"On my "} <a target="_blank" href="https://github.com/MonkyMars">GitHub</a>, {"you can find a showcase of the projects I’ve developed. Each project represents not just a learning milestone but also a step towards mastering the craft of software development. I believe that the best way to grow as a developer is through hands-on experience, and my GitHub portfolio reflects my commitment to continuous learning and innovation."}</p>
+            <p>{"On my "} <Link target="_blank" href="https://github.com/MonkyMars">{'GitHub'}</Link>, {"you can find a showcase of some of my projects I’ve developed. Each project I've tried to implement something new and different to expand my skillset as a developer."}</p>
 
-            <p>{"Looking ahead, I’m excited to continue my journey in software engineering, exploring new technologies, and contributing to meaningful projects. Whether it’s developing the next big web app or collaborating with other passionate developers, I’m driven by the desire to create impactful digital experiences that make a difference."}</p>
+            <p>{"I speak English and Dutch both fluently so in the future I look forward to meeting fellow developers from all over the planet and settling in a team!"}</p>
 
-            <code><section>const </section>mainLanguages <section>{"="}</section>{"["}<section>{'"JavaScript", "html", "css"'}</section>{"]"}</code>
-            <code><section>const</section>frameworks <section>{"="}</section> {"["}<section>{'"Next.js", "React"'}</section>{"]"}</code>
+            <code><section>{'const'} </section>{'mainLanguages'} <section>{"="}</section>{"["}<section>{'"JavaScript", "html", "css"'}</section>{"]"}</code>
+            <code><section>{'const'}</section>{'frameworks'} <section>{"="}</section> {"["}<section>{'"Next.js", "React"'}</section>{"]"}</code>
           </div>
         </main>
       </div>
