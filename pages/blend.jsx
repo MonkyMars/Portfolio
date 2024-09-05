dccb58a1b2020d43215827412b3d8d7f090feefb
MonkyMars <155177968+MonkyMars@users.noreply.github.com>
2024-09-05 17:13:57 +0200

    fixed nav on contact page

--- pages/blend.jsx
+++ pages/blend.jsx
@@ -86,9 +86,9 @@ export default function Blend() {
 
             <p>{"Over the past two years, I’ve dedicated myself to learn coding, I mainly focus on the front-end. My journey as a software developer began when I was 12 and wanted to create my first Discord Bot, since then I've improved a lot at coding, day in and day out spent on coding new web-apps"}</p>
 
-            <p>{"I specialize in the front-end and a little back-end, making sure that every project I work on looks good and works as intended. My experience includes developing multiple fully-functional websites, creating a database-API."}</p>
+            <p>{"I specialize in the front-end and a little back-end, making sure that every project I work on looks professional and works as intended. My experience includes developing multiple fully-functional websites, creating a database-API."}</p>
 
-            <p>{"I’m open to learning and improving my skills, whether it’s by exploring a new framework, or finding better solutions to my problems"}</p>
+            <p>{"I’m open to learning and would like to improve my skills, whether it’s by exploring a new framework, or finding better solutions to my problems"}</p>
 
             <p>{"On my "} <Link target="_blank" href="https://github.com/MonkyMars">{'GitHub'}</Link>, {"you can find a showcase of some of my projects I’ve developed. Each project I've tried to implement something new and different to expand my skillset as a developer."}</p>
 
