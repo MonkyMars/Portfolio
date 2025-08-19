e7c2897e69b2623342625b03e289cf2581ab750b
Levi Noppers <levinoppers@proton.me>
2025-08-19 14:30:12 +0200

    Changed location to be less specific and added linux as a like card; also introduded bun to the project

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -3,80 +3,80 @@ import { Mail, MapPinHouse } from "lucide-react";
 import Link from "next/link";
 
 const Aside = () => (
-	<aside
-		className="md:col-span-4 lg:col-span-3 shadow-lg h-full"
-		id="aside"
-	>
-		<div className="bg-white dark:bg-slate-900/95 dark:text-gray-100 rounded-2xl shadow-lg p-4 sm:p-3 sm:py-4 border border-gray-100 dark:border-gray-800 h-full flex flex-col max-w-sm mx-auto">
-			{/* Header Section */}
-			<div className="text-center mb-4">
-				<h1 className="text-xl text-gray-800 dark:text-gray-100 mb-1 font-doto font-extrabold">
-					Levi Noppers
-				</h1>
-				<p className="text-primary-600 font-medium text-base">
-					Fullstack Web Developer
-				</p>
-			</div>
+  <aside className="md:col-span-4 lg:col-span-3 shadow-lg h-full" id="aside">
+    <div className="bg-white dark:bg-slate-900/95 dark:text-gray-100 rounded-2xl shadow-lg p-4 sm:p-3 sm:py-4 border border-gray-100 dark:border-gray-800 h-full flex flex-col max-w-sm mx-auto">
+      {/* Header Section */}
+      <div className="text-center mb-4">
+        <h1 className="text-xl text-gray-800 dark:text-gray-100 mb-1 font-doto font-extrabold">
+          Levi Noppers
+        </h1>
+        <p className="text-primary-600 font-medium text-base">
+          Fullstack Web Developer
+        </p>
+      </div>
 
-			{/* GitHub Button */}
-			<Link
-				href="https://www.github.com/monkymars"
-				prefetch
-				target="_blank"
-				className="font-doto font-extrabold w-full flex items-center justify-center gap-2 
+      {/* GitHub Button */}
+      <Link
+        href="https://www.github.com/monkymars"
+        prefetch
+        target="_blank"
+        className="font-doto font-extrabold w-full flex items-center justify-center gap-2
                 bg-gradient-to-r from-gray-700 to-gray-800 text-white py-2.5 px-3 rounded-xl transform
-                transition-all duration-300 shadow-md hover:shadow-lg text-base hover:-translate-y-[2px] 
+                transition-all duration-300 shadow-md hover:shadow-lg text-base hover:-translate-y-[2px]
                 dark:from-slate-900 dark:to-gray-800 mb-4"
-			>
-				<span className="flex items-center gap-2">
-					GitHub
-					<Image
-						src="/icons/github.png"
-						alt="github"
-						width={20}
-						height={20}
-						className="invert opacity-90 hover:opacity-100"
-					/>
-				</span>
-			</Link>
+      >
+        <span className="flex items-center gap-2">
+          GitHub
+          <Image
+            src="/icons/github.png"
+            alt="github"
+            width={20}
+            height={20}
+            className="invert opacity-90 hover:opacity-100"
+          />
+        </span>
+      </Link>
 
-			{/* Contact Information */}
-			<div className="space-y-4 flex-1">
-				<Link
-					className="block p-4 rounded-2xl hover:bg-gray-50 dark:hover:bg-slate-800/50 transition-all cursor-pointer"
-					href="mailto:levi.laptop@hotmail.com"
-					prefetch
-					target="_blank"
-				>
-					<div className="flex items-center gap-3 text-gray-700 dark:text-gray-100 mb-1">
-						<Mail className="text-primary-500" size={20} />
-						<span className="text-base font-doto font-extrabold">Email</span>
-					</div>
-					<p
-						className="text-sm text-gray-600 pl-8 text-wrap break-words dark:text-gray-300/90"
-						title="Email"
-					>
-						Levi.laptop@hotmail.com
-					</p>
-				</Link>
+      {/* Contact Information */}
+      <div className="space-y-4 flex-1">
+        <Link
+          className="block p-4 rounded-2xl hover:bg-gray-50 dark:hover:bg-slate-800/50 transition-all cursor-pointer"
+          href="mailto:levi.laptop@hotmail.com"
+          prefetch
+          target="_blank"
+        >
+          <div className="flex items-center gap-3 text-gray-700 dark:text-gray-100 mb-1">
+            <Mail className="text-primary-500" size={20} />
+            <span className="text-base font-doto font-extrabold">Email</span>
+          </div>
+          <p
+            className="text-sm text-gray-600 pl-8 text-wrap break-words dark:text-gray-300/90"
+            title="Email"
+          >
+            Levi.laptop@hotmail.com
+          </p>
+        </Link>
 
-				<Link
-					className="block p-4 rounded-2xl hover:bg-gray-50 dark:hover:bg-slate-800/50 transition-all cursor-pointer"
-					href="https://maps.app.goo.gl/DaZ11s9p4dsVNVjn7"
-					prefetch
-					target="_blank"
-				>
-					<div className="flex items-center gap-3 text-gray-700 mb-1 dark:text-gray-100">
-						<MapPinHouse className="text-primary-500" size={20} />
-						<span className="text-base font-doto font-extrabold">Location</span>
-					</div>
-					<p className="text-sm text-gray-600 pl-8 w-full dark:text-gray-300/90" title="Location">
-						The Hague, The Netherlands
-					</p>
-				</Link>
-			</div>
-		</div>
-	</aside>
+        <Link
+          className="block p-4 rounded-2xl hover:bg-gray-50 dark:hover:bg-slate-800/50 transition-all cursor-pointer"
+          href="https://www.google.com/maps/place/Nederland"
+          prefetch
+          target="_blank"
+        >
+          <div className="flex items-center gap-3 text-gray-700 mb-1 dark:text-gray-100">
+            <MapPinHouse className="text-primary-500" size={20} />
+            <span className="text-base font-doto font-extrabold">Location</span>
+          </div>
+          <p
+            className="text-sm text-gray-600 pl-8 w-full dark:text-gray-300/90"
+            title="Location"
+          >
+            The Netherlands, Europe
+          </p>
+        </Link>
+      </div>
+    </div>
+  </aside>
 );
 
 export default Aside;
--- app/components/Likes.tsx
+++ app/components/Likes.tsx
@@ -1,67 +1,75 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Likes = () => {
-	return (
-		<section
-			className="bg-white dark:bg-slate-900/95 rounded-2xl shadow-lg p-8 mb-8 border-1 border-[rgba(255,255,255, 0.1)] border border-gray-100 dark:border-gray-800"
-			id="likes"
-		>
-			<h2 className="text-2xl font-doto font-extrabold text-gray-800 dark:text-gray-100 mb-6 flex items-center gap-2">
-				Things I like
-				<div className="h-1 w-24 bg-primary-500 rounded-full"></div>
-			</h2>
-			<div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
-				<LikesCard
-					label="Go"
-					iconSrc="go"
-					note="I love coding in Go. It's currently my favorite language due to the simplicity and speed."
-				/>
-				<LikesCard
-					label="Music"
-					iconSrc="spotify"
-					note="I love listening to music while coding."
-				/>
-			</div>
-		</section>
-	)
+  return (
+    <section
+      className="bg-white dark:bg-slate-900/95 rounded-2xl shadow-lg p-8 mb-8 border-1 border-[rgba(255,255,255, 0.1)] border border-gray-100 dark:border-gray-800"
+      id="likes"
+    >
+      <h2 className="text-2xl font-doto font-extrabold text-gray-800 dark:text-gray-100 mb-6 flex items-center gap-2">
+        Things I like
+        <div className="h-1 w-24 bg-primary-500 rounded-full"></div>
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
+        <LikesCard
+          label="Go"
+          iconSrc="go"
+          note="I love coding in Go. It's currently my favorite language due to the simplicity and speed."
+        />
+        <LikesCard
+          label="Music"
+          iconSrc="spotify"
+          note="I love listening to music while coding."
+        />
+        <LikesCard
+          label="Linux"
+          iconSrc="linux"
+          note="I enjoy using Linux for its flexibility and control over my PC. I personally use CachyOS:)"
+          link="https://github.com/MonkyMars/dotfiles"
+        />
+      </div>
+    </section>
+  );
 };
 
 interface LikesCardProps {
-	label: string;
-	iconSrc: string;
-	note: string;
-};
+  label: string;
+  iconSrc: string;
+  note: string;
+  link?: string;
+}
 
-const invertedIcons: string[] = [
-	"spotify"
-]
+const invertedIcons: string[] = ["spotify"];
 
-const LikesCard = ({
-	label,
-	iconSrc,
-	note,
-}: LikesCardProps) => {
+const LikesCard = ({ label, iconSrc, note, link }: LikesCardProps) => {
+  const isInverted = invertedIcons.includes(iconSrc);
+  const imageClass = "object-contain transition-transform duration-300";
+  const iconClass = isInverted ? `${imageClass} dark:invert` : imageClass;
 
-	const isInverted = invertedIcons.includes(iconSrc);
-	const imageClass = "object-contain transition-transform duration-300";
-	const iconClass = isInverted ? `${imageClass} dark:invert` : imageClass;
-
-	return (
-		<div className="flex flex-col items-center justify-center text-center bg-white dark:bg-slate-900/90 rounded-2xl shadow-lg p-6 border-1 border-[rgba(255,255,255, 0.1)] border border-gray-100 dark:border-gray-800 h-full">
-			<div className="relative w-16 h-16 mb-4 flex items-center justify-center">
-				<Image
-					src={`/icons/${iconSrc}.png`}
-					alt={label}
-					width={48}
-					height={48}
-					className={iconClass}
-					priority
-				/>
-			</div>
-			<h3 className="text-lg font-bold text-gray-800 dark:text-gray-100 mb-2">{label}</h3>
-			<p className="text-gray-600 dark:text-gray-400 text-sm">{note}</p>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center justify-center text-center bg-white dark:bg-slate-900/90 rounded-2xl shadow-lg p-6 border-1 border-[rgba(255,255,255, 0.1)] border border-gray-100 dark:border-gray-800 h-full">
+      <div className="relative w-16 h-16 mb-4 flex items-center justify-center">
+        <Image
+          src={`/icons/${iconSrc}.png`}
+          alt={label}
+          width={48}
+          height={48}
+          className={iconClass}
+          priority
+        />
+      </div>
+      <h3 className="text-lg font-bold text-gray-800 dark:text-gray-100 mb-2">
+        {label}
+      </h3>
+      <p className="text-gray-600 dark:text-gray-400 text-sm">{note}</p>
+      {link && (
+        <Link href={link} className="text-blue-600 dark:text-blue-400 text-sm">
+          My dotfiles
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default Likes;
